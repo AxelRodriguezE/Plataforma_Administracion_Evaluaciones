f27ddf2fda055d88eb2e46e7c3809903f68228b8
nikooo2h <nicolas.o.h@hotmail.com>
2014-01-05 12:54:59 -0300

    editar asignatura

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/~AxelRodriguez/Administracion_Evaluaciones';
+$config['base_url']	= 'http://localhost/nico/Plataforma_Administracion_Evaluaciones';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/administrativo/editar_asignatura.php
+++ application/views/administrativo/editar_asignatura.php
@@ -1,7 +1,63 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<div class="container">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="alert alert-success">    
+        <?php
+        $id_asignatura = $query->id_asignatura;
+        $codigo_asignatura = $query->codigo_asignatura;
+        $seccion_asignatura = $query->seccion_asignatura;
+        $nombre_asignatura = $query->nombre_asignatura;
+        
+        
+        $id = array(
+            'type' => 'hidden',
+            'id' => 'id',
+            'name' => 'id',
+            'value' => $id_asignatura
+        );
+        $codigo = array(
+            'type' => 'text',
+            'id' => 'codigo',
+            'name' => 'codigo',
+            'class' =>'form-control',
+            'value' => $codigo_asignatura 
+        );
+        $seccion = array(
+            'type' => 'text',
+            'id' => 'seccion',
+            'name' => 'seccion',
+            'class' => 'form-control',
+            'value' => $seccion_asignatura
+        );
+        $nombre = array(
+            'type' => 'text',
+            'id' => 'nombre',
+            'name' => 'nombre',
+            'class' =>'form-control',
+            'value' => $nombre_asignatura
+        );
+          
+        
+        
+        $button = array(
+            'class' => 'btn btn-primary',
+            'value' => 'Modificar'
+        );
+        echo form_open(base_url('/index.php/asignatura/editar'));
+        echo form_fieldset('Editar Asignatura');
+            echo $query->nombre_academico .' '. $query->apellidos_academico;
+            echo '<br>';
+            echo form_label('Codigo Asignatura:');
+            echo form_input($codigo);
+            echo form_label('Seccion Asignatura:');
+            echo form_input($seccion);
+            echo form_label('Nombre Asignatura:');
+            echo form_input($nombre);
+            echo form_input($id);
+            echo '<br>';
+            echo form_submit($button);
+        echo form_fieldset_close();
+        echo form_close();
+    ?>
+        </div>
+    </div>
+</div>
